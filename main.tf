6fd4675db099ed90af45ff956c4ac48db6542097
Henk <hbokhoven@gmail.com>
2021-07-17 16:52:10 +0200

    Update playlist

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "spotify_playlist" "playlist" {
   public      = true
 
   tracks = [
-    data.spotify_track.siouxie.id,
+    data.spotify_track.siouxsie.id,
     data.spotify_track.aneka.id,
     data.spotify_track.hot_cold.id,
     data.spotify_track.lio.id,
@@ -25,15 +25,19 @@ resource "spotify_playlist" "playlist" {
     data.spotify_track.bbq_band.id,
     data.spotify_track.robyn.id,
     data.spotify_track.bananarama.id,
+    data.spotify_track.joe_jackson.id,
+    data.spotify_track.valerie_lagrange.id,
     data.spotify_search_track.by_kraftwerk.tracks[0].id,
     data.spotify_search_track.by_royksopp.tracks[0].id,
     data.spotify_search_track.by_styx.tracks[0].id,
+    data.spotify_search_track.by_rhcp.tracks[0].id,
   ]
 }
 
-# Track:  Siouxsie and the Banshees - Israel (1980)
-data "spotify_track" "siouxie" {
+# Track: Siouxsie and the Banshees - Israel (1980)
+data "spotify_track" "siouxsie" {
   spotify_id = "6jLmHKaHW6Cy37Gp5eTD3i"
+  # The Spotify ID can be taken from the track's URL.
   # url = "https://open.spotify.com/track/6jLmHKaHW6Cy37Gp5eTD3i?si=e6f23e76f11d4711"
 }
 
@@ -50,7 +54,6 @@ data "spotify_track" "hot_cold" {
 # Track: Lio - Amoureux Solitaires (1980)
 data "spotify_track" "lio" {
   spotify_id = "0ZJgIjGJdYzgb03LSH09Gx"
-  # url = "https://open.spotify.com/track/0ZJgIjGJdYzgb03LSH09Gx?si=d383776523cd4760"
 }
 
 # Track: Simple Minds - I Travel (1980)
@@ -73,6 +76,16 @@ data "spotify_track" "bananarama" {
   url = "https://open.spotify.com/track/4dW0Xy4pIeY5jqskF3BKif?si=98865176029b4a56"
 }
 
+# Track: Joe Jackson & Elaine Caswell - Happy ending (1984)
+data "spotify_track" "joe_jackson" {
+  spotify_id = "1qv9TWDpXh50DPLgWD4bnH"
+}
+
+# Track: Valerie Lagrange - La Folie (1983)
+data "spotify_track" "valerie_lagrange" {
+  spotify_id = "6YWAoH1TauvkO2x4qpCr0w"
+}
+
 data "spotify_search_track" "by_kraftwerk" {
   artists = ["Kraftwerk"]
   #album   = ""
@@ -81,14 +94,20 @@ data "spotify_search_track" "by_kraftwerk" {
 
 data "spotify_search_track" "by_royksopp" {
   artists = ["Royksopp"]
-  #album   = ""
-  name = "The Girl and the Robot"
+  album   = "Junior"
+  name    = "The Girl and the Robot"
 }
 
 data "spotify_search_track" "by_styx" {
   artists = ["Styx"]
-  #album   = ""
-  name = "Mr. Roboto"
+  album   = "Kilroy Was Here"
+  name    = "Mr. Roboto"
+}
+
+data "spotify_search_track" "by_rhcp" {
+  artists = ["Red Hot Chili Peppers"]
+  album   = "The Getaway"
+  name    = "Go Robot"
 }
 
 output "tracks" {
@@ -96,5 +115,6 @@ output "tracks" {
     data.spotify_search_track.by_kraftwerk.tracks,
     data.spotify_search_track.by_royksopp.tracks,
     data.spotify_search_track.by_styx.tracks,
+    data.spotify_search_track.by_rhcp.tracks,
   ]
 }
